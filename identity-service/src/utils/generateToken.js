456f748d588be212affee1be93fb0478ad650b85
suarim <muhammadsuarim@gmail.com>
2025-03-31 12:54:44 +0530

    enhance token generation to return both auth token and refresh token, and save refresh token to database

--- identity-service/src/utils/generateToken.js
+++ identity-service/src/utils/generateToken.js
@@ -1,6 +1,18 @@
 const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+const refreshtoken = require('../models/RefreshToken');
 const generateToken = async (user)=>{
-    const token = await jwt.sign({id:user._id,name:user.name},process.env.JWT_SECRET,{expiresIn:'1h'});
-    return token;
+    const authtoken = await jwt.sign({id:user._id,name:user.name},process.env.JWT_SECRET,{expiresIn:'1h'});
+    const refreshtoken = await crypto.randomBytes(32).toString('hex');
+    const expiresAt = new Date(Date.now() + 1000*60*60*24*30);
+    const refreshtokenmodel = new refreshtoken({
+        token:refreshtoken,
+        user:user._id,
+        expiresAt   
+    })
+    await refreshtokenmodel.save();
+    return {authtoken,refreshtoken};
+
+
 }
 module.exports = generateToken;
