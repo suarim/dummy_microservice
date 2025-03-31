51c98c38f0104272a8b04e4d5099dec35795941c
suarim <muhammadsuarim@gmail.com>
2025-03-31 09:52:38 +0530

    add logging functionality using winston for better error handling and server status reporting

--- identity-service/src/server.js
+++ identity-service/src/server.js
@@ -2,13 +2,15 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const logger = require('./utils/Logger');
 const app = express();
 app.use(cors());
 app.use(express.json());
 console.log(process.env.MONGO_URI);app.listen(process.env.PORT,()=>{
+    logger.info(`Server is running on port ${process.env.PORT}`);   
 mongoose.connect(process.env.MONGO_URI).then(()=>{
-    console.log('Connected to MongoDB');
+    logger.info('Connected to MongoDB');
 }).catch((err)=>{
-    console.log(err);
+    logger.error(err);
 })
 })
