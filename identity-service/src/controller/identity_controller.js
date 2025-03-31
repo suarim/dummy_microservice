3dc49752605cd7362484f7244410aa715314d8e9
suarim <muhammadsuarim@gmail.com>
2025-03-31 11:09:54 +0530

    add validation for user registration input and create validation utility

--- identity-service/src/controller/identity_controller.js
+++ identity-service/src/controller/identity_controller.js
@@ -6,12 +6,19 @@ const userregistercontroller = async (req,res)=>{
     try{
         const {name,email,password} = req.body;
         const hashedPassword = await argon2.hash(password);
+        const {error} = validateschema(req.body);
+        if (error){
+            logger.error(error);
+            return res.status(400).json({status:'error',message:error.details[0].message});
+        }
+        else{
         const user = new usermodel({
             name,email,hashedPassword
         })
         await user.save();
         logger.info(`User registered with email ${email}`);
         res.status(201).json({status:'success',message:'User registered successfully'});
+    }
     }
     catch(err){
         logger.error(err);
