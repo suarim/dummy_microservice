9967fa72738e244db8d8fa64928996aca20b9b33
suarim <muhammadsuarim@gmail.com>
2025-04-06 14:29:54 +0530

    feat: implement logging and error handling middleware for API gateway

---
+++ api-gateway/src/server.js
@@ -0,0 +1,17 @@
+const express = require('express'); 
+const logger = require('./utils/Logger');
+const errorhandler = require('./middleware/errorHandler');
+const cors = require('cors');
+const helmet = require('helmet');
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use((req,res,next)=>{
+    console.log('Request received');
+    logger.info(`${req.method} ${req.url}`);
+    next();
+})
+app.listen(process.env.PORT || 3000, () => {
+    logger.info(`Server is running on port ${process.env.PORT || 3000}`);
+})
