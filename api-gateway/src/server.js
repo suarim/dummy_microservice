43950ce304355fd4422d03004f47d9b308af5cff
suarim <muhammadsuarim@gmail.com>
2025-04-06 14:33:07 +0530

    feat: add error handling middleware and request logging to server

--- api-gateway/src/server.js
+++ api-gateway/src/server.js
@@ -8,11 +8,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
+
 app.use((req,res,next)=>{
     console.log('Request received');
     logger.info(`${req.method} ${req.url}`);
     next();
 })
+app.use(errorhandler)
 app.listen(process.env.PORT || 3000, () => {
     logger.info(`Server is running on port ${process.env.PORT || 3000}`);
 })
