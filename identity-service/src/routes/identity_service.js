c2c924d8a38a5f13c1a810d5a529b2b2e6b34903
suarim <muhammadsuarim@gmail.com>
2025-04-13 02:54:36 +0530

    feat: add refresh token and logout functionality to identity service

--- identity-service/src/routes/identity_service.js
+++ identity-service/src/routes/identity_service.js
@@ -1,7 +1,10 @@
 const express = require('express');
-const {userregistercontroller,getallusers} = require('../controller/identity_controller');
+const {userregistercontroller,getallusers,refreshtokenController,logoutController} = require('../controller/identity_controller');
 
 const router = express.Router();
 router.post('/register',userregistercontroller)
 router.get('/users',getallusers)
+router.post('/refresh-token',refreshtokenController)
+router.post('/logout',logoutController)
+
 module.exports = router;
